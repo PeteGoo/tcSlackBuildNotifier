8ebfbdf459e0694a6db1fab583d19e3bdd24fe9c
PeteGoo <pete@petegoo.com>
2014-09-02 23:03:58 +1200

    Fixing nul ref. version bump

--- tcslackbuildnotifier-core/src/main/java/slacknotifications/teamcity/payload/content/SlackNotificationPayloadContent.java
+++ tcslackbuildnotifier-core/src/main/java/slacknotifications/teamcity/payload/content/SlackNotificationPayloadContent.java
@@ -213,7 +213,7 @@ public class SlackNotificationPayloadContent {
             Loggers.SERVER.debug("SlackNotificationPayloadContent :: Could not get Branch Info by calling sRunningBuild.getBranch(). Probably an old version of TeamCity");
         }
         setBuildStatusUrl(server.getRootUrl() + "/viewLog.html?buildTypeId=" + getBuildTypeId() + "&buildId=" + getBuildId());
-        String branchSuffix = getBranchIsDefault() ? "" : " [" + getBranchDisplayName() + "]";
+        String branchSuffix = getBranchIsDefault() != null && getBranchIsDefault() ? "" : (" [" + getBranchDisplayName() + "]");
         setBuildDescriptionWithLinkSyntax(String.format("<" + getBuildStatusUrl() + "|" + sRunningBuild.getBuildType().getFullName().toString() + " #" + sRunningBuild.getBuildNumber() + branchSuffix + ">"));
         setBuildStateDescription(buildState.getDescriptionSuffix());
         setRootUrl(server.getRootUrl());
