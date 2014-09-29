91c6c380d069240a8d48e7d9120ce642513ea7e1
PeteGoo <pete@petegoo.com>
2014-09-29 19:48:18 +1300

    Removing redundant test

--- tcslackbuildnotifier-core/src/test/java/slacknotifications/teamcity/settings/SlackNotificationProjectSettingsTest.java
+++ tcslackbuildnotifier-core/src/test/java/slacknotifications/teamcity/settings/SlackNotificationProjectSettingsTest.java
@@ -25,10 +25,6 @@ public class SlackNotificationProjectSettingsTest {
 		SlackNotificationProjectSettingsFactory psf = new SlackNotificationProjectSettingsFactory(psm);
 		psf.createProjectSettings("project1");
 	}
-	
-	@Test
-	public void TestSettings(){
-		
-	}
+
 	
 }
