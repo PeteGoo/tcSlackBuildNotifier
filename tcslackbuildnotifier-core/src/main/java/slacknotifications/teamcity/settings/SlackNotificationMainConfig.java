6c067f5780c51ad194a8a18e9d36c4bcf6c3bcb8
petegoo <pete@petegoo.com>
2015-02-07 23:34:16 +1300

    :lipstick:

--- tcslackbuildnotifier-core/src/main/java/slacknotifications/teamcity/settings/SlackNotificationMainConfig.java
+++ tcslackbuildnotifier-core/src/main/java/slacknotifications/teamcity/settings/SlackNotificationMainConfig.java
@@ -13,11 +13,6 @@ import slacknotifications.teamcity.Loggers;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SlackNotificationMainConfig implements ChangeListener {
     public final String DEFAULT_BOTNAME = "TeamCity";
