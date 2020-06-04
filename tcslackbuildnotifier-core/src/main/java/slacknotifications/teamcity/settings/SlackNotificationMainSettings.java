596c279112eb7271fa3e75570154fa98587565fa
Eugene Akhmetkhanov <axmetishe+github@gmail.com>
2020-06-04 17:02:00 +0300

    [Notification conditions] Rework plugin packaging
    Remove ant tasks
    Make getPluginVersion work with manifest instead of file

--- tcslackbuildnotifier-core/src/main/java/slacknotifications/teamcity/settings/SlackNotificationMainSettings.java
+++ tcslackbuildnotifier-core/src/main/java/slacknotifications/teamcity/settings/SlackNotificationMainSettings.java
@@ -7,11 +7,8 @@ import org.jdom.Element;
 import slacknotifications.SlackNotificationProxyConfig;
 import slacknotifications.teamcity.Loggers;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.jar.Manifest;
 
 public class SlackNotificationMainSettings implements MainConfigProcessor {
 	private static final String NAME = SlackNotificationMainSettings.class.getName();
@@ -154,9 +151,9 @@ public class SlackNotificationMainSettings implements MainConfigProcessor {
         if(version != null){
             return version;
         }
-        Properties props = new Properties();
-        props.load(SlackNotificationMainSettings.class.getResourceAsStream("/version.txt"));
-        version = props.getProperty("version");
+        Manifest props = new Manifest();
+        props.read(SlackNotificationMainSettings.class.getResourceAsStream("/META-INF/MANIFEST.MF"));
+        version = props.getMainAttributes().getValue("Build-Version");
         return version;
     }
 }
